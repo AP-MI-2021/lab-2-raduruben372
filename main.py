577b37c455c21b7963d79cbfaf7c84a04f3d6f82
Your Name <ruben.radu@stud.ubbcluj.ro>
2021-10-06 08:55:07 -0700

    Lab 2

--- main.py
+++ main.py
@@ -20,10 +20,12 @@ def get_base_16_from_2(n):
         n_10 //= 16
     return n_16
 
+
 def test_get_base_16_from_2():
     assert get_base_16_from_2(11010010) == "D2"
     assert get_base_16_from_2(100) == '4'
     assert  get_base_16_from_2(1001) == '9'
+
 
 def get_n_choose_k(n,k):
     '''
@@ -57,20 +59,47 @@ def get_n_choose_k(n,k):
         comb = fact_n // (fact_nk * fact_k)
         return comb
 
+
 def test_get_n_choose_k():
     assert get_n_choose_k(1,2) == 0
     assert get_n_choose_k(2,1) == 2
     assert get_n_choose_k(10,5) == 252
 
 
-def main():
+def get_largest_prime_below(n):
+    if n == 2 or n == 1:
+        return 0
+    while True:
+        n -= 1
+        t = True
+        for i in range(2,n):
+            if n % i == 0:
+                t = False
+        if t is True:
+            return n
+
+
+def test_get_largest_prime_below():
+    assert get_largest_prime_below(15) == 13
+    assert get_largest_prime_below(53) == 47
+    assert get_largest_prime_below(2) == 0
+    assert get_largest_prime_below(17) == 13
+
+
+def test():
     test_get_n_choose_k()
     test_get_base_16_from_2()
+    test_get_largest_prime_below()
+
+
+def main():
+    test()
     ok = '0'
     while ok != 'x':
         print('''
 1.Transformă un număr dat din baza 2 în baza 16. Numărul se dă în baza 2.
 2.Calculează combinări de n luate câte k (n și k date).
+3.Găsește ultimul număr prim mai mic decât un număr dat.
 x.Iesire
         ''')
         ok = input('NR: ')
@@ -85,6 +114,12 @@ x.Iesire
                 print('N >= K!!!')
             else:
                 print('Rezultat = ', get_n_choose_k(n,k))
+        elif ok == '3':
+            n = int(input("Nr:"))
+            if get_largest_prime_below(n) == 0:
+                print('Nu exista numar prim mai mic')
+            else:
+                print('Rezultat: ',get_largest_prime_below(n))
 
 if __name__ == '__main__':
     main()
